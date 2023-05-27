d153384db0610043bd932393a0cfaf0328a8173d
Maximilian Huber <gh@maxhbr.de>
2023-05-27 21:45:15 +0200

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- download_firmware.sh
+++ download_firmware.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+DIR="$(readlink -f "$(dirname "$0")")/firmware"
+cd "$DIR"
+
+date_dir="$(date "+%F_%H-%M-%S")"
+mkdir -p "$DIR/$date_dir"
+cd "$DIR/$date_dir"
 gh run download --pattern firmware
+
+cd "$DIR"
+find "$date_dir" -type f |
+    while read output; do
+        bn="$(basename "$output")"
+        if [[ -f "$bn" ]]; then
+            rm "$bn"
+        fi
+        ln -s "$output" "$bn"
+    done
