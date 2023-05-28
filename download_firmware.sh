72a20332277f6b6f1d83dd067e67588952b11056
Maximilian Huber <gh@maxhbr.de>
2023-05-28 18:27:55 +0200

    work on mykeeb_v7a2 config
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- download_firmware.sh
+++ download_firmware.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 DIR="$(readlink -f "$(dirname "$0")")/firmware"
 cd "$DIR"
 
+ gh run list --limit 1
+
 date_dir="$(date "+%F_%H-%M-%S")"
 mkdir -p "$DIR/$date_dir"
 cd "$DIR/$date_dir"
@@ -14,6 +16,7 @@ cd "$DIR"
 find "$date_dir" -type f |
     while read output; do
         bn="$(basename "$output")"
+        echo "get $bn"
         if [[ -f "$bn" ]]; then
             rm "$bn"
         fi
