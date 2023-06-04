5797595d984e89a54a5cfc6e489ab4d69f7ddd4a
Maximilian Huber <gh@maxhbr.de>
2023-06-04 18:27:40 +0200

    test positional hold
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- download_firmware.sh
+++ download_firmware.sh
@@ -5,7 +5,13 @@ set -euo pipefail
 DIR="$(readlink -f "$(dirname "$0")")/firmware"
 cd "$DIR"
 
- gh run list --limit 1
+gh run list --limit 1
+conclusion="$(gh run list --limit 1 --json conclusion --jq '.[].conclusion')"
+echo "last action run: ${conclusion}"
+if [[ "$conclusion" != "success" ]]; then
+    exit 1
+fi
+echo
 
 date_dir="$(date "+%F_%H-%M-%S")"
 mkdir -p "$DIR/$date_dir"
