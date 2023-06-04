859659d31a7176079f0a689bb3a679f4233da1b6
Maximilian Huber <gh@maxhbr.de>
2023-06-04 16:20:10 +0200

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- flash_firmeware.sh
+++ flash_firmeware.sh
@@ -5,9 +5,11 @@ label=XIAO-SENSE
 dev=/dev/disk/by-label/"$label"
 firmware="$1"
 
+echo "waiting for $dev"
 while sleep 1; do
     if [[ -e "$dev" ]]; then
         set -x
+        sleep 1
         udisksctl mount -b "$dev"
         mnt="$(findmnt -nr -o target -S "$dev")"
         cp "$firmware" "$mnt"
